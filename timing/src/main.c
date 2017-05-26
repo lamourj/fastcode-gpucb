ce683f4da02f682a4f98f6ebf3818bd5ffe2d8c7
Linus Handschin <hlinus@stud24-h56.inf.ethz.ch>
2017-05-26 15:00:23 +0200

    timing supports validation to baseline

--- timing/src/main.c
+++ timing/src/main.c
@@ -10,23 +10,54 @@
 
 
 // ONLY CHANGE THIS
-#include "../../src/gpucb3.h"
+#include "../../src/gpucb5.h"
 const int N_MIN = 200; // min meshgrid size
-const int N_MAX = 200; // max meshgrid size
+const int N_MAX = 200; // max meshgrid size. MUST BE REACHABLE WITH STEP
 const int N_STEP = 100; // 
 const int ITER_MIN = 5; // min number of iterations
-const int ITER_MAX = 6; // min number of iterations
-const int ITER_MAX = 100; // min number of iterations
+const int ITER_MAX = 10; // max number of iterations. MUST BE REACHABLE WITH STEP
 const int ITER_STEP = 5; // min number of iterations
-const int NUM_RUNS = 3;
+const int NUM_RUNS = 1;
 const int COMPARE_TO_BASELINE = 1; // 0 or 1
 
 // DO NOT CHANGE THE FOLLOWING LINES
-void validatate(const int, const int, double);
+
+void validate(const int II, const int NN, double cycles_measured){
+	printf("validate for I=%d, N=%d \n", II, NN);
+	initialize_bl(II,NN);
+	cycles_count_start();
+	for (int j = 0; j < NUM_RUNS; j += 1) run_bl();
+	uint64_t cycle_cnt = cycles_count_stop();
+	double tot_mu_diff = 0.0;
+	double tot_sigma_diff = 0.0;
+
+	for(int ii=0; ii<II;ii++){
+		double diff = mu_bl[ii] - mu_[ii];
+		double abs = (diff < 0) ? -diff : diff;
+		tot_mu_diff += abs;
+	}
+
+	for(int ii=0; ii<II;ii++){
+		double diff = sigma_bl[ii] - sigma_[ii];
+		double abs = (diff < 0) ? -diff : diff;
+		tot_sigma_diff += abs;
+	}
+	if(tot_mu_diff > 0 || tot_sigma_diff > 0){
+		printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+		printf(" DOES NOT VALIDATE! difference mu: %lf, difference simga: %lf!\n", tot_mu_diff, tot_sigma_diff);
+		printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+	} else{
+		printf("==============================================================\n");
+		printf("VALIDATED: SPEEDUP: %lf                                  \n", (double) cycles_measured/ ((double)cycle_cnt / NUM_RUNS));
+		printf("==============================================================\n");
+	}
+	clean();
+	clean_bl();
+}
 
 int main() {
 	if(!(N_STEP % 8 == 0) || !(N_MIN % 8 == 0)) {
-        	printf("n or step is not divisible by 4 !!! \n");
+        	printf("n or step is not divisible by 8 !!! \n");
 	}
 	int j, n, i;
 	uint64_t cycle_cnt;
@@ -55,7 +86,7 @@ int main() {
 			cycles_count_start();
 			for (j = 0; j < NUM_RUNS; j += 1) run();
 			cycle_cnt = cycles_count_stop();
-			clean();
+			if(n < N_MAX || COMPARE_TO_BASELINE==0) clean();
 			fprintf(fp, "%d\t%d\t%lf\n", n, i, (double) cycle_cnt / NUM_RUNS);
 		}
 		fclose(fp);
@@ -87,7 +118,7 @@ int main() {
 			cycles_count_start();
 			for (j = 0; j < NUM_RUNS; j += 1) run();
 			cycle_cnt = cycles_count_stop();
-			clean();
+			if(i < ITER_MAX || COMPARE_TO_BASELINE==0) clean();
 			fprintf(fp, "%d\t%d\t%lf\n", n, i, (double) cycle_cnt / NUM_RUNS);
 		}
 		fclose(fp);
@@ -108,35 +139,4 @@ int main() {
     return 0;
 }
 
-void validate(const int II, const int NN, double cycles_measured){
-	printf("validate for I=%d, N=%d", II, NN);
-	initialize(II,NN);
-	cycles_count_start();
-	for (int j = 0; j < NUM_RUNS; j += 1) run();
-	uint64_t cycle_cnt = cycles_count_stop();
-	double tot_mu_diff = 0.0;
-	double tot_sigma_diff = 0.0;
 
-	for(int ii=0; ii<II;ii++){
-		double diff = mu_bl[ii] - mu_[ii];
-		double abs = (diff < 0) ? -diff : diff;
-		tot_mu_diff += abs;
-	}
-
-	for(int ii=0; ii<II;ii++){
-		double diff = sigma_bl[ii] - sigma_[ii];
-		double abs = (diff < 0) ? -diff : diff;
-		tot_sigma_diff += abs;
-	}
-	if(tot_mu_diff > 0 || tot_sigma_diff > 0){
-		printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-		printf("! DOES NOT VALIDATE! difference mu: %lf, difference simga: %lf!", tot_mu_diff, tot_sigma_diff);
-		printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-	} else{
-		printf("==============================================================");
-		printf("= VALIDATED: SPEEDUP: %lf                                  =", (double) cycles_measured/cycle_cnt);
-		printf("==============================================================");
-	}
-
-	clean_bl();
-}
